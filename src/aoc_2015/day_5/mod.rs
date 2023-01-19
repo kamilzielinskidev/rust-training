186293352bc3f2c907ad38c1fcc2938e4d770b7e
kamilzielinskidev <kamilzielinskidev@gmail.com>
2023-01-19 17:22:27 +0100

    aoc_2015 day_5 part_2 solution

--- src/aoc_2015/day_5/mod.rs
+++ src/aoc_2015/day_5/mod.rs
@@ -77,3 +77,60 @@ pub fn solution_first_part() -> Result<usize, Box<dyn error::Error>> {
 
     Ok(amount_of_nice_words)
 }
+
+fn check_pairs_requirement(word: &String) -> bool {
+    let mut has_twice_pair = false;
+
+    for index in 0..word.len() - 1 {
+        if has_twice_pair {
+            break;
+        }
+
+        let pair = word.get(index..index + 2).unwrap();
+        let rest_first_part = word.get(0..index).unwrap().to_string();
+        let rest_second_pard = word.get(index + 2..word.len()).unwrap().to_string();
+
+        if rest_first_part.contains(pair) || rest_second_pard.contains(pair) {
+            has_twice_pair = true;
+        };
+    }
+
+    has_twice_pair
+}
+
+fn check_pairs_with_middle_char(word: &String) -> bool {
+    let mut has_same_letters_with_another_between = false;
+
+    for index in 0..word.len() - 1 {
+        if has_same_letters_with_another_between {
+            break;
+        }
+
+        let first = word.chars().nth(index);
+        let third = word.chars().nth(index + 2);
+
+        if first == third {
+            has_same_letters_with_another_between = true;
+        };
+    }
+
+    has_same_letters_with_another_between
+}
+
+pub fn solution_second_part() -> Result<usize, Box<dyn error::Error>> {
+    let file_string = read_to_string("data")?.trim().to_string();
+
+    let words: Vec<String> = file_string
+        .split_ascii_whitespace()
+        .map(String::from)
+        .collect();
+
+    let amount_of_nice_words = words.iter().fold(0, |acc, next_word| {
+        if check_pairs_requirement(next_word) && check_pairs_with_middle_char(next_word) {
+            return acc + 1;
+        }
+        acc
+    });
+
+    Ok(amount_of_nice_words)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use aoc_2015::day_5::solution_first_part;
+use aoc_2015::day_5::solution_second_part;
 
 pub mod aoc_2015;
 
 fn main() {
-    println!("{:?}", solution_first_part())
+    println!("{:?}", solution_second_part())
 }
